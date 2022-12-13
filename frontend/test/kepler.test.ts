0547b1a541c891ea4f4415dd1cc885cef71e07dc
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-12-13 09:19:17 -0800

    github pages

---
+++ frontend/test/kepler.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest"
+
+test("kepler", () => {
+    expect(true).toBe(true)
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,15 @@ import solidPlugin from "vite-plugin-solid"
 
 export default defineConfig({
     plugins: [solidPlugin()],
+    base: "/kepler/",
     server: {
         port: 3000,
+        host: "0.0.0.0",
     },
     build: {
         target: "esnext",
+    },
+    resolve: {
+        conditions: ["development", "browser"],
     },
 })
