64a01c0391e31705e95ac5fea66c493eff5a54ca
Matt Zhang <set.stun@gmail.com>
2022-12-13 00:48:56 -0500

    Added base robot

---
+++ backend/chat_session/robots/base_robot.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+
+from chat_session.backends import Backend, EmptyBackend
+
+
+class Robot:
+    def __init__(self, uuid: UUID):
+        self.uuid = uuid
+        self.backend = EmptyBackend()
+
+    def attach_backend(self, backend: Backend):
+        self.backend = backend
+
+    def prompt(self, prompt: str) -> str:
+        response = self.backend.query(prompt)
+        return response
