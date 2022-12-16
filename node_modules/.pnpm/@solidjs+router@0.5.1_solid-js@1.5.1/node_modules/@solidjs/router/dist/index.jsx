73fbb6c6b9b20183ab4574c8a2739cb6592fcb9e
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-12-15 17:27:28 -0800

    ignore node_modules

--- node_modules/.pnpm/@solidjs+router@0.5.1_solid-js@1.5.1/node_modules/@solidjs/router/dist/index.jsx
+++
@@ -1,5 +0,0 @@
-export * from "./components";
-export * from "./integration";
-export * from "./lifecycle";
-export { useRouteData, useHref, useIsRouting, useLocation, useMatch, useNavigate, useParams, useResolvedPath, useSearchParams, useBeforeLeave, } from "./routing";
-export { mergeSearchString as _mergeSearchString } from "./utils";
--- node_modules/.pnpm/@solidjs+router@0.5.1_solid-js@1.5.1/node_modules/@solidjs/router/dist/lifecycle.d.ts
+++
@@ -1,2 +0,0 @@
-import { BeforeLeaveLifecycle } from "./types";
-export declare function createBeforeLeave(): BeforeLeaveLifecycle;
