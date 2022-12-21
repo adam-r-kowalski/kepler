aef7fe986dd10c6d5274aa76d9c286e7c2fed51c
Adam Kowalski <adam.kowalski.work@gmail.com>
2022-12-20 18:59:40 -0800

    format the received message from markdown to html

--- src/Received/Received.tsx
+++ src/Received/Received.tsx
@@ -1,11 +1,24 @@
-import { JSXElement } from "solid-js"
+import { marked } from "marked"
+import hljs from "highlight.js/lib/core"
+import javascript from "highlight.js/lib/languages/javascript"
+import python from "highlight.js/lib/languages/python"
+import "highlight.js/styles/vs.css"
 
 import style from "./Received.module.css"
 
+hljs.registerLanguage("javascript", javascript)
+hljs.registerLanguage("python", python)
+
 interface Props {
-    children: JSXElement
+    children: string
 }
 
 export const Received = (props: Props) => {
-    return <div class={style.received}>{props.children}</div>
+    marked.setOptions({
+        highlight: function (code) {
+            return hljs.highlightAuto(code).value
+        },
+    })
+    const markdown = marked(props.children)
+    return <div class={style.received} innerHTML={markdown} />
 }
